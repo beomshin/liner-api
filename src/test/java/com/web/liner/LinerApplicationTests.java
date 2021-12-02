208c17423261d33ad79116666879e4a2e412f267
김범신 <chzh859@naver.com>
2021-12-03 00:55:46 +0900

    프로젝트생성

---
+++ src/test/java/com/web/liner/LinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.liner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
